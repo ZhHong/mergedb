03890d31b035c85f1c17c63e29f539cb29b9c816
Monkey <371816216@qq.com>
2016-06-03 00:12:29 +0800

    init

---
+++ src/mergedb.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "time"
+func main(){
+	//get mysql connect
+	//get offset
+	//clean unuse
+	//merge logdb
+	//clean procdb
+	//merge procdb
+	//clean other data
+	//reset global data
+}
